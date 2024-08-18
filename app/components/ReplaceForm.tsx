d39ce221594e03a08bdd0c84a19e94b2c4553022
HeyRupam <rupamb881@gmail.comm>
2024-08-18 21:16:59 +0530

    fixed rows type error

--- app/components/ReplaceForm.tsx
+++ app/components/ReplaceForm.tsx
@@ -50,7 +50,7 @@ const ReplaceForm: FC<ReplaceFormProps> = ({ onSubmit }) => {
       </div>
       <div className="mb-5">
         <label htmlFor="copyright" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Copyright</label>
-        <textarea value={copyright} onChange={e => setCopyright(e.target.value)} id="copyright" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." required></textarea>
+        <textarea value={copyright} onChange={e => setCopyright(e.target.value)} id="copyright" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..." required></textarea>
       </div>
       <div className="mb-5">
         <input type='checkbox' name='route' onChange={(e) => setRoute(e.target.checked)} checked={route} /> <label htmlFor='route'>Use Route</label>
